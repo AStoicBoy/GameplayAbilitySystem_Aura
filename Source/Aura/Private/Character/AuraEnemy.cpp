2185f867bbf12a8888c8a2b38fda25492947193a
Dorian Souc <dorian.souc@gmail.com>
2024-12-28 16:14:50 +0100

    Bases Classes Creation

---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright Dorian Souc
+
+
+#include "Character/AuraEnemy.h"
+
---
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,17 @@
+// Copyright Dorian Souc
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
