b7d53c2e2513679019697244b1671f341beddf18
Dorian Souc <dorian.souc@gmail.com>
2025-01-01 19:47:10 +0100

    Potion Battle WIP (ActorEffect)

---
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -0,0 +1,34 @@
+// Copyright Dorian Souc
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AuraEffectActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class AURA_API AAuraEffectActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AAuraEffectActor();
+	// virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweppResult);
+	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+};
