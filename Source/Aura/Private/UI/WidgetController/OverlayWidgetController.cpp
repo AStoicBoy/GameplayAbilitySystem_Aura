8c41c4acc468f68bc1dcf5137ad8cc1753f811e6
Dorian Souc <dorian.souc@gmail.com>
2025-03-05 20:29:23 +0100

    Message Displaying when pot collected but only for duration effect. Looking for a solution that works for Instant Effects. This is due to newer version of Unreal Engine differing form the one used in the course.

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -25,14 +25,24 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetMaxHealthAttribute()).AddUObject(this, &UOverlayWidgetController::MaxHealthChanged);
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetManaAttribute()).AddUObject(this, &UOverlayWidgetController::ManaChanged);
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetMaxManaAttribute()).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
+	
 Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
 	[this](const FGameplayTagContainer& AssetTags)
 	{
 		for (const FGameplayTag& Tag : AssetTags)
 		{
-			const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
-			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, Msg);
-			TObjectPtr<FUIWidgetRow> Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable,Tag);
+			// Determine if this tag matches TagToCheck, expanding out parent tags
+			//"A.1".MatchesTag("A") will return True, "A".MatchesTag("A.1") will return False
+			// For example: Message.HealthPotion "Message.HealthPotion".MatchesTag("Message") will return True, "Message".MatchesTag("Message.HealthPotion") will return False
+
+			FGameplayTag MessageTag = FGameplayTag::RequestGameplayTag(FName("Message"));
+			if (Tag.MatchesTag(MessageTag))
+			{
+				const FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable,Tag);
+				MessageWidgetRowDelegate.Broadcast(*Row);
+			}
+			
+	
 		}
 	}
 	
